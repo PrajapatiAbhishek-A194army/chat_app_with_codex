2ef75bb54837e98c7d4adcce80da5a05e4d6bd04
Abhishek prajapati <prajapativishnuu@gmail.com>
2026-05-30 15:36:18 +0530

    cmploted

--- backend/server.js
+++ backend/server.js
@@ -110,6 +110,4 @@ initializeSocket(server, allowedOrigins);
 
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
-  console.log(`Allowed CORS origins: ${allowedOrigins.join(", ")}`);
-  console.log("Google Client ID:", process.env.VITE_GOOGLE_CLIENT_ID);
 });
